502f767522c8a255e81dc1a2819ed27f919c16e4
Marcelo Hitoshi <Marcelo_Hitoshi@MacBook-Pro-de-Marcelo.local>
2026-02-24 10:35:01 -0300

    função para enviar eventos

--- src/componentes/FormularioDeEvento/index.jsx
+++ src/componentes/FormularioDeEvento/index.jsx
@@ -7,8 +7,21 @@ import { TituloFormulario } from "../TituloFormulario";
 import "./formulario-de-eventos.estilos.css";
 
 export function FormularioDeEvento({ temas }) {
+  function aoFormSubmetido(formData) {
+    console.log("Opa, tá na hora de criar um novo evento", formData);
+    const evento = {
+      capa: formData.get("capa"),
+      tema: temas.find(function (item) {
+        return item.id == formData.get("tema");
+      }),
+      data: new Date(formData.get("dataEvento")),
+      titulo: formData.get("nomeEvento"),
+    };
+    console.log("Esse é o evento", evento);
+  }
+
   return (
-    <form className="form-evento">
+    <form className="form-evento" action={aoFormSubmetido}>
       <TituloFormulario>Preencha para criar um evento:</TituloFormulario>
 
       <div className="campos">
